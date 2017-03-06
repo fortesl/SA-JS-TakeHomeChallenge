970d5ef717365e266a2aaecd75e25bd0c77abb55
fortesl <fortesl@yahoo.com>
2017-03-06 14:05:44 -0600

    initial commit

--- ngApp/src/app/main/main.controller.js
+++ ngApp/src/app/main/main.controller.js
@@ -1,14 +1,45 @@
+(function () {
+  'use strict';
+
+  angular.module('ngApp').factory('DataService', ['$http', function($http) {
+
+    return {
+      get:function() { return $http.get('data/data.json'); }
+    };
+
+  }]);
+})();
+
 (function() {
   'use strict';
 
+  var debug = true;
+
   angular
     .module('ngApp')
-    .controller('MainController', MainController);
+    .controller('MainController', ['DataService', MainController]);
 
   /** @ngInject */
-  function MainController() {
+  function MainController(DataService) {
     var vm = this;
     vm.title = "Sa Take Home Challenge";
     vm.subTitle = "Build something great for us :-)";
+    vm.data = [];
+
+    //get data
+    var init = function() {
+      DataService.get().then(function(response) {
+        var property = response.data.property;
+        for (var i=0; i<7; i++) {
+          vm.data.push(property);
+        }
+        if (debug) {
+          console.log(vm.data);
+        }
+      });
+    };
+
+    init();
   }
 })();
+
